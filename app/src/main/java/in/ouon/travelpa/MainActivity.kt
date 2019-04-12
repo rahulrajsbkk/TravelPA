14517a21ae1a4244ee2cb49930aa6232e03a89d3
smiley <rahulrajsbkk@gmail.com>
2019-04-12 12:03:34 +0530

    Connected Firebase

--- app/src/main/java/in/ouon/travelpa/MainActivity.kt
+++ app/src/main/java/in/ouon/travelpa/MainActivity.kt
@@ -1,12 +1,35 @@
 package `in`.ouon.travelpa
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    internal var morefirebaseDatabase: FirebaseDatabase? = null
+    internal var moreRef: DatabaseReference? = null
+    private val mcontext: Context? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.setLayoutManager(GridLayoutManager(mcontext,2));
+        AddtoCart.setOnClickListener{
+//            startActivity(Intent(this@MainActivity, CartActivity::class.java))
+            Toast.makeText(this@MainActivity, "Cart Clicked.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+
     }
 }
