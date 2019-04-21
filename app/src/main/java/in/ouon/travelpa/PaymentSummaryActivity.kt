07cdbcd2afdcb7b87e97f847cee873381be75d8a
z3r0c00l-2k <mjack1327@gmail.com>
2019-04-21 13:33:49 +0530

    Payment Summary Activity UI

---
+++ app/src/main/java/in/ouon/travelpa/PaymentSummaryActivity.kt
@@ -0,0 +1,12 @@
+package `in`.ouon.travelpa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PaymentSummaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_summary)
+    }
+}
