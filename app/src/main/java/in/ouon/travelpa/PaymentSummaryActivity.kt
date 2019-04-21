5fbd3851f3a439b1dece6b72375b01df4208c111
z3r0c00l-2k <mjack1327@gmail.com>
2019-04-21 13:49:42 +0530

    Work around on Payment Summary

--- app/src/main/java/in/ouon/travelpa/PaymentSummaryActivity.kt
+++ app/src/main/java/in/ouon/travelpa/PaymentSummaryActivity.kt
@@ -1,12 +1,20 @@
 package `in`.ouon.travelpa
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_payment_summary.*
 
 class PaymentSummaryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_summary)
+
+        btHome.setOnClickListener { v ->
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
     }
 }
